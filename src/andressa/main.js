fee19714b6e49ef5d5ce73ffb0313b98e2797fa3
prograMadorJ <jorge.fernando.brito@gmail.com>
2024-01-17 23:46:57 -0300

    excertcios criados

---
+++ src/andressa/main.js
@@ -0,0 +1,41 @@
+/*
+
+    Escreva um código para somar alguns números
+
+    1 + 2 + 5 + 6
+
+    depois use o mesmo para:
+    subtrair 
+    multiplicar
+    dividir
+
+
+*/
+
+// entrada e declarando variaveis
+const numero1 = 1
+const numero2 = 2
+const numero3 = 3
+
+// processamento
+const resultado = numero1 + numero2 + numero3
+
+
+// saida
+console.log(resultado)
+
+
+// escovar bits
+
+
+
+
+
+//
+
+
+
+//
+
+
+
---
+++ src/exemplo/main.js
@@ -0,0 +1,40 @@
+/*
+
+    Escreva um código para somar alguns números
+
+    1 + 2 + 5 + 6
+
+    depois use o mesmo para:
+    subtrair 
+    multiplicar
+    dividir
+
+
+*/
+
+// entrada e declarando variaveis
+
+const numeros = [1,2,3]
+
+// processamento
+const resultado = numeros[0] + numeros[1] + numeros[2]
+
+
+// saida
+console.log(resultado)
+
+
+
+
+
+
+
+
+//
+
+
+
+//
+
+
+
