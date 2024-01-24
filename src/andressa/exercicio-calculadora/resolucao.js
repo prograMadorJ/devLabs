273561efb1ce710dc06ee5fe7b8a2c623173aa7d
prograMadorJ <jorge.fernando.brito@gmail.com>
2024-01-23 23:27:23 -0300

    organizando pastas

---
+++ src/andressa/exercicio-calculadora/resolucao.js
@@ -0,0 +1,26 @@
+const num1 = 1;
+const num2 = 2;
+const num3 = 5;
+const num4 = 6;
+
+resultSoma = num1 + num2 + num3 + num4;
+
+
+function calculadora (x, y, texto) {
+    const soma = x + y;
+    console.log(`${soma} ${texto} soma`);
+
+    const divisao = x / y;
+    console.log(`${divisao}  ${texto} divisao`);
+
+    const multiplicacao = x * y;
+    console.log(`${multiplicacao} ${texto} multiplicacao`);
+
+    const subtracao = x - y;
+    console.log(`${subtracao}  ${texto} subtracao`)
+};
+
+
+
+
+ 
