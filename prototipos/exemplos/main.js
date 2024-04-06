bc5177e542875eaa8418e3e2517c385aaa242cf9
prograMadorJ <jorge.fernando.brito@gmail.com>
2024-04-05 23:57:57 -0300

    exemplos de classe

---
+++ prototipos/exemplos/main.js
@@ -0,0 +1,21 @@
+// Paradgma de programação
+// Orientado a Objeto
+
+class Carro {
+   constructor(modelo, placa, velocidade, quantRodas) {
+    this.modelo = modelo
+    this.placa = placa
+    this.velocidade = velocidade
+    this.quantRodas = quantRodas
+   }
+}
+
+
+const c1 = new Carro("SEDAN","12321","300", "2")
+
+c1.modelo = "ALTERADO sdf"
+
+
+
+
+
