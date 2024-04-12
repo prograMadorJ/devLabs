1989c9ab524cff7a1f05255aa1c3942b964772bd
prograMadorJ <jorge.fernando.brito@gmail.com>
2024-04-11 22:10:49 -0300

    codigo da andressa

--- prototipos/andressa/main.js
+++ prototipos/andressa/main.js
@@ -1,20 +1,39 @@
-class medicamento {
-    Codigo = ""
-    nomeComercial = ""
-    nomeGenerico = ""
-    codCategoria = ""
-    lote = ""
-    validade = ""
-    preco = ""
-    precoDesconto = ""
-    dataCompra = ""
-    codFornecedor= ""
-    fonteCompra = ""
-    dosagem = ""
-    quantidade = ""
-    descricao = ""
-    codLaboratorio = ""
-    tipoDeUso = ""
-    dataInício = ""
-    previsaoTerminoCaixa = ""
+class Medicamento {
+    constructor(codigo, nomeComercial, nomeGenerico, categoria, lote, validade, preco, precoDesconto, dataCompra, codFornecedor, fonteCompra, dosagem, quantidade, descricao, codLaboratorio, tipoUso, dataInicio, previsaoTermino) {
+        this.codigo = codigo;
+        this.nomeComercial = nomeComercial;
+        this.nomeGenerico = nomeGenerico;
+        this.categoria = categoria;
+        this.lote = lote;
+        this.validade = validade;
+        this.preco = preco;
+        this.precoDesconto = precoDesconto;
+        this.dataCompra = dataCompra;
+        this.codFornecedor = codFornecedor;
+        this.fonteCompra = fonteCompra;
+        this.dosagem = dosagem;
+        this.quantidade = quantidade;
+        this.descricao = descricao;
+        this.codLaboratorio = codLaboratorio;
+        this.tipoUso = tipoUso;
+        this.dataInicio = dataInicio;
+        this.previsaoTermino = previsaoTermino;
+    }
+}
+
+class Laboratorio {
+    constructor(codigo, nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+}
+
+class Fornecedor {
+    constructor(codigo, nome, endereco, telefone, site) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.site = site;
+    }
 }
