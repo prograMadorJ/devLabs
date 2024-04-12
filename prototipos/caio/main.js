f7a8e4e361725249fa5fc66f80f81ce937a303ee
caiocfdalmeida <ccardoso.judo@gmail.com>
2024-04-11 22:16:13 -0300

    22:16

--- prototipos/caio/main.js
+++ prototipos/caio/main.js
@@ -13,3 +13,18 @@ class aluno {
 }
 
 }
+
+class responsavel {
+    
+    constructor (codigo, nome, rg, telefone, endereco, dataUltimoExame, dataCadastro) {
+    this.codigo = codigo
+    this.nome = nome
+    this.dataNascimento = dataNascimento
+    this.codFaixa = codFaixa
+    this.codResponsavel = codResponsavel
+    this.dataUltimoExame = dataUltimoExame
+    this.dataCadastro = dataCadastro
+
+}
+
+}
