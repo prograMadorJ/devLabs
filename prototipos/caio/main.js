5596ae536910746c781382b565717091df9cac6c
caiocfdalmeida <ccardoso.judo@gmail.com>
2024-04-11 21:58:47 -0300

    Olá de novo

--- prototipos/caio/main.js
+++ prototipos/caio/main.js
@@ -1,7 +1,15 @@
 
 class aluno {
+    
+    constructor (codigo, nome, dataNascimento, codFaixa, codResponsavel, dataUltimoExame, dataCadastro) {
     this.codigo = codigo
     this.nome = nome
     this.dataNascimento = dataNascimento
-    this.codFaixa
+    this.codFaixa = codFaixa
+    this.codResponsavel = codResponsavel
+    this.dataUltimoExame = dataUltimoExame
+    this.dataCadastro = dataCadastro
+
+}
+
 }
