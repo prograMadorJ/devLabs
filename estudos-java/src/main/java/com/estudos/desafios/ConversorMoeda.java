429c788e652fe10f964c14d1884fe9c05146f621
jorge <->
2024-06-15 20:50:11 -0300

    importacao das bilbiotecas

--- estudos-java/src/main/java/com/estudos/desafios/ConversorMoeda.java
+++ estudos-java/src/main/java/com/estudos/desafios/ConversorMoeda.java
@@ -1,5 +1,10 @@
 package com.estudos.desafios;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.json.JSONObject;
+
 public class ConversorMoeda implements ConversaoFinanceira {
 
     private double taxaCambio;
@@ -18,7 +23,32 @@ public class ConversorMoeda implements ConversaoFinanceira {
     }
 
     public static void main(String[] args) {
-        System.out.println("ok");
-    }
+        String url = "https://api.apilayer.com/exchangerates_data/latest";
+        try {
 
+            HttpResponse<JsonNode> response = Unirest.get(url)
+                    .header("accept", "application/json")
+                    .header("apiKey", "1twwZkAVmKJySlvcwd6O0d5T2Ay3gsIK")
+                    .queryString("base", "USD")
+                    .queryString("symbols", "BRL")
+                    // .field("parameter", "value")
+                    // .field("firstname", "Gary")
+                    .asJson();
+
+            if (response.getStatus() == 200) {
+                JSONObject json = new JSONObject(response.getBody());
+                double cambio = json.getJSONObject("rates").getDouble("BRL");
+
+                ConversorMoeda conversorMoeda = new ConversorMoeda();
+                conversorMoeda.setTaxaCambio(cambio);
+                System.out.println("\n");
+                System.out.println("Resultado da Conversao de Moedas");
+                System.out.println(conversorMoeda.converterDolarParaReal(5));
+            } else {
+                throw new RuntimeException("Falha ao obter a cotação do dólar.");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
